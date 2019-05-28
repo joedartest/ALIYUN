133045a6d56c92c7f7346fdd5e7da3c5c4ad7a26
joedar <joedar@qq.com>
2019-05-28 10:16:39 +0800

    提交CUSTOM文件夹

---
+++ CUSTOM/js/front/header.partLeft.logo.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="tab home">
+		<div><a class="logo" href="/" title="首页"></a></div>
+	</div>
+</template>
---
+++ CUSTOM/js/front/header.partLeft.menu.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="tab menu">
+		<div>
+			<input type="checkbox" class="none" id="HEADERMENU">
+			<div class="openMenu"><label for="HEADERMENU"><b></b><b></b><b></b></label></div>
+			<div class="HEADERMENU">
+				<div class="MENU">
+					<div class="POPCLOSE"><label for="HEADERMENU"><b></b><b></b></label></div>
+					<ul class="clearfix">
+						<li><a class="HMENU" href="/autism/page/subject" data-name="subject"><span>话题</span></a></li>
+						<li><a class="HMENU" href="/autism/page/tutorial" data-name="tutorial"><span>教程</span></a></li>
+						<li><a class="HMENU" href="/autism/page/trend" data-name="trend"><span>动态</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
